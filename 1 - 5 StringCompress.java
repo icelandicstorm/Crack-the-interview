ea083a13204e216492b5661cca00cad1a715e711
zhang xijing <xijing2000@Macintosh-104.local>
2013-11-16 19:41:14 -0600

    add 1 - 5

---
+++ 1 - 5 StringCompress.java
@@ -0,0 +1,49 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: xijing2000
+ * Date: 11/16/13
+ * Time: 2:24 08PM
+ */
+public class StringCompress {
+
+    public static String StringCompress(String string){
+       if(string == "") return "String cannot be null!";
+
+       String compress = "";
+       int count = 1;
+       char old = string.charAt(0);
+       compress = compress + old;
+
+       for(int i=1; i<string.length(); i++){
+           char newchar = string.charAt(i);
+
+           if(old == newchar){
+               count++;
+           }
+
+           else if (old != newchar){
+               compress = compress + count;
+               count = 1;
+               compress = compress + newchar;
+               old = newchar;
+           }
+        }
+
+       if(compress.substring(compress.length()-1) != Integer.toString(count)) {
+            compress = compress + count;
+       }
+
+       if(compress.length() < string.length()){
+           return compress;
+       }
+       else{
+           return string;
+       }
+
+    }
+
+    public static void main(String args[]){
+         String string = "aaa";
+         System.out.println(StringCompress(string));
+    }
+}
